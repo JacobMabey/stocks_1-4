9da33961bd21072edfc99d6ff0557c58c0f04053
JacobMabey <mabey.jacob@gmail.com>
2022-10-11 15:46:45 -0600

    test changes 4

--- src/main/java/edu/neumont/csc180/mabey/jacob/App.java
+++ src/main/java/edu/neumont/csc180/mabey/jacob/App.java
@@ -9,5 +9,6 @@ public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a test!");
+        System.out.println("Another test is required!");
     }
 }
