745975e16ea3d4907bc665a69df15636ac323b43
JacobMabey <mabey.jacob@gmail.com>
2022-10-12 12:05:37 -0600

    Added ability to get account info from JSON

--- src/main/java/edu/neumont/csc180/mabey/jacob/Account.java
+++ src/main/java/edu/neumont/csc180/mabey/jacob/Account.java
@@ -1,9 +1,14 @@
 package edu.neumont.csc180.mabey.jacob;
 
+import java.util.ArrayList;
+
 public class Account {
     private int accountNum;
     private String firstname, lastname;
     private String ssn, email, phoneNum;
+    private String cash;
+
+    private ArrayList<Transaction> transactions;
 
     public Account(int accountNum, String firstname, String lastname, String ssn, String email, String phoneNum) {
         this.accountNum = accountNum;
@@ -12,7 +17,20 @@ public class Account {
         this.ssn = ssn;
         this.email = email;
         this.phoneNum = phoneNum;
-        
+        this.cash = "$0.00";
+
+        transactions = new ArrayList<Transaction>();
+    }
+    public Account(int accountNum, String firstname, String lastname, String ssn, String email, String phoneNum, String cash) {
+        this.accountNum = accountNum;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ssn = ssn;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.cash = cash;
+
+        transactions = new ArrayList<Transaction>();
     }
     
 }
--- src/main/java/edu/neumont/csc180/mabey/jacob/StockMain.java
+++ src/main/java/edu/neumont/csc180/mabey/jacob/StockMain.java
@@ -12,19 +12,48 @@ import org.json.simple.parser.ParseException;
  * Hello world!
  */
 public final class StockMain {
+    private static JSONParser parser = new JSONParser();
+    private static JSONArray jsonArray;
+
     private StockMain() {
     }
     public static void main(String[] args) {
+        InitializeList();
+
         System.out.println(GetJSONValue(300, "account_number"));
     }
 
+    public static Account GetAccount(int accountNum) {
+        if (accountNum < 1 || accountNum > jsonArray.size()) {
+            System.out.println("account number does not exist");
+            return null;
+        }
+        int account_number = Integer.parseInt(GetJSONValue(accountNum, "account_number"));
+        String first_name = GetJSONValue(accountNum, "first_name");
+        String last_name = GetJSONValue(accountNum, "last_name");
+        String ssn = GetJSONValue(accountNum, "ssn");
+        String email = GetJSONValue(accountNum, "email");
+        String phoneNum = GetJSONValue(accountNum, "phone");
+        String cash = GetJSONValue(accountNum, "beginning_balance");
 
-    public static String GetJSONValue(int accountNum, String key) {
+        return new Account(account_number, first_name, last_name, ssn, email, phoneNum, cash);
+    }
+
+
+    private static String GetJSONValue(int accountNum, String key) {
         String value = "";
+        int arrayLength = jsonArray.size();
+        if (accountNum < 1 || accountNum > arrayLength) {
+            System.out.println("account number does not exist");
+            return value;
+        }
+        value = (String)((JSONObject)jsonArray.get(accountNum - 1)).get(key).toString();
+        return value;
+    }
+
+    private static void InitializeList() {
         try {
-            JSONParser parser = new JSONParser();
-            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("stock_transations-3.by.account.holder.json"));
-            value = (String)((JSONObject)jsonArray.get(accountNum - 1)).get(key).toString();
+            jsonArray = (JSONArray) parser.parse(new FileReader("stock_transations-3.by.account.holder.json"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("The file could not be found");
@@ -35,6 +64,5 @@ public final class StockMain {
             e.printStackTrace();
             System.out.println("The file could not be parsed correctly");
         }
-        return value;
     }
 }
